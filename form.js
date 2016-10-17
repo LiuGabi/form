ba631350a1c7f86229dbf5c2bcf53c79292e614b
Gabi <gabi@eyebuydirect.com>
2016-10-17 18:08:31 +0800

    update

--- form.js
+++ form.js
@@ -1,31 +1,80 @@
-$.fn.uiForm = function(options) {
+(function($) {
 
-	var opts = $.extends({}, $.fn.uiForm.defaults, options);
+	// $.fn.uiForm.defaults = {};
 
-	var form = {
+	$.fn.uiForm = function(options) {
 
-		init: function() {
 
-			console.log('aa');
+		var opts = $.extend({}, $.fn.uiForm.defaults, options || {});
 
-		},
+		var parent = $(this);
 
-		activeForm: function() {
+		var form = {
 
-			console.log('message');
+			isEmpty: function(els) {
 
-		}
+				if (!els) {
+					return;
+				}
+
+				if (!$(els).val()) {
+
+					$(els).removeClass(opts.filledClass)
+					return true;
+
+				} else {
+
+					$(els).addClass(opts.filledClass)
+					return false;
+
+				}
+
+			},
+
+			isActive: function(els) {
+
+				if (!els) {
+					return;
+				}
+
+				var activeClass = $(els).is(opts.activeClass);
+
+				if (!activeClass) {
+
+					return false;
+
+				} else {
+
+					return true;
+
+				}
+
+			}
+
+		};
+
+		var init = function() {
+
+			parent.on('click touchstart', function(e) {
+
+				e.stopPropagation();
+
+				$(this).parent().siblings().children('.' + opts.activeClass).removeClass(opts.activeClass);
+				$(this).addClass(opts.activeClass);
+
+			});
+
+			$('html').not(parent.is('.' + opts.activeClass)).on('click touchstart', function(e) {
+
+				e.stopPropagation();
+				parent.removeClass(opts.activeClass);
+
+			});
+
+		};
+
+		init();
 
 	};
 
-	form.init();
-
-	return this;
-
-};
-
-$.fn.uiForm.defaults = {
-
-
-
-};
+})(jQuery);
